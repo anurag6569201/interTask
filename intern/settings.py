3587c92481abb4c17c7120b003dd645de4beff56
anurag singh <anurag6569201@gmail.com>
2024-03-16 01:17:27 +0530

    added mysql db and improved ui

--- intern/settings.py
+++ intern/settings.py
@@ -83,12 +83,23 @@ WSGI_APPLICATION = 'intern.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'intern_db',
+        'USER':'root',
+        'PASSWORD':'6569as@AS',
+        'HOST':'127.0.0.1',
+        'POST':'3306'
     }
 }
+
 
 
 # Password validation
