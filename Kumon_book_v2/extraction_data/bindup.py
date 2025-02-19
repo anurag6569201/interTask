f460b6902b7e60b664577dfe565b4f477778c042
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2025-02-19 15:33:00 +0530

    updated kumonv2

--- Kumon_book_v2/extraction_data/bindup.py
+++ Kumon_book_v2/extraction_data/bindup.py
@@ -2,16 +2,16 @@ import os
 import pandas as pd
 from PIL import Image
 import sys
-import os
-from PIL import Image
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(script_dir)
 
-from folder_paths import current_folder_paths
-saving_folder = os.path.join(script_dir,'./material/combined_pdf')
 def images_to_pdf():
-    # Load and preprocess CSV data
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(script_dir)
+    from folder_paths import current_folder_paths
+    
+    saving_folder = os.path.join(script_dir, './material/combined_pdf')
+    if not os.path.exists(saving_folder):
+        os.makedirs(saving_folder)
     csv_path = os.path.join(script_dir, '../urls.csv')
     data_df = pd.read_csv(csv_path)
 
@@ -23,24 +23,20 @@ def images_to_pdf():
         folder_name = os.path.join(folder_name, 'generated')
         print(folder_name)
 
-        # Get all images sorted in order
         image_files = sorted(
-            [f for f in os.listdir(folder_name) if f.endswith(".png")], 
+            [f for f in os.listdir(folder_name) if f.endswith(".png")],
             key=lambda x: int(x[1:-4])
         )
 
         if not image_files:
             print("No images found in the folder.")
-        else:
-            output_pdf = os.path.join(saving_folder, f"{file_name}.pdf") # Ensure output path is set before saving
-            
-            # Open first image
-            first_image = Image.open(os.path.join(folder_name, image_files[0]))
-            image_list = [Image.open(os.path.join(folder_name, img)).convert("RGB") for img in image_files[1:]]
+            continue
 
-            # Save as PDF
-            first_image.save(output_pdf, save_all=True, append_images=image_list)
-            print(f"PDF created successfully: {output_pdf}")
+        output_pdf = os.path.join(saving_folder, f"{file_name}.pdf")
+        first_image = Image.open(os.path.join(folder_name, image_files[0]))
+        image_list = [Image.open(os.path.join(folder_name, img)).convert("RGB") for img in image_files[1:]]
+
+        first_image.save(output_pdf, save_all=True, append_images=image_list)
+        print(f"PDF created successfully: {output_pdf}")
 
 
-images_to_pdf()
